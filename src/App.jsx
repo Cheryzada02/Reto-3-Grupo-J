f9557f56f8c999793e75aab9069a75975dbea556
Chery M. Viola Martinez <137524454+Cheryzada02@users.noreply.github.com>
2026-04-28 19:34:49 -0400

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { insert_into_user_profile } from './authentication/db_functions'
-import User_form from './components/testing2';
-import Supplier_page from './components/Testing_Supplier';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import CustomerService from "./pages/CustomerService";
+import SupplierPage from "./components/Testing_Supplier";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
 
-  return <Supplier_page/>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servicio-cliente" element={<CustomerService />} />
+        <Route path="/suplidores" element={<SupplierPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
